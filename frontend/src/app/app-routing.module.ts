aa8668d28c6f704118e2107ebed19b66873876bc
adrielfragapane <adrielfragapane@users.noreply.github.com>
2020-05-11 22:05:47 -0300

    Incorporación de mapas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { PropuestaComponent } from './components/propuesta/propuesta.component';
+import { DetallePropuestaComponent } from './components/detalle-propuesta/detalle-propuesta.component';
+import { MapComponent } from './components/map/map.component';
 
 
 const routes: Routes = [
-  {path: '', component: UsuarioComponent},
   {path: 'usuarios', component: UsuarioComponent},
-  {path: 'propuestas', component: PropuestaComponent}
+  {path: 'propuestas', component: PropuestaComponent},
+  {path: 'propuesta', component: DetallePropuestaComponent},
+  {path: 'mapa', component: MapComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleMapsModule } from '@angular/google-maps'
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { PropuestaComponent } from './components/propuesta/propuesta.component';
+import { DetallePropuestaComponent } from './components/detalle-propuesta/detalle-propuesta.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuarioComponent,
-    PropuestaComponent
+    PropuestaComponent,
+    DetallePropuestaComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
